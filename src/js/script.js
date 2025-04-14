6973506827836406b72fb6df03a279881916b79c
Konstantin <korchikov_k@mail.ru>
2025-04-14 11:50:44 +0300

    feat: add works

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,34 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import "/src/sass/style.scss";
+
+try {
+  new Swiper('.works__slider', {
+    loop: true,
+    navigation: {
+      nextEl: ".icon-right-open",
+      prevEl: ".icon-left-open"
+    },
+    breakpoints: {
+      360: {
+        slidesPerView: 1
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 5
+      },
+      1920: {
+        spaceBetween: 35
+      }
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    modules: [Navigation, Pagination]
+  });
+} catch (e) { }
